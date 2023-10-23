a2f0df489613debc9d7d9f28959e9fdf788cc9a1
Fernando Jose Amoedo Miguez <ferjamoedom@gmail.com>
2023-10-23 11:13:47 +0200

    añadida funcionalidad a cronometro y timer

--- src/app/herramientas/crono/crono.page.ts
+++ src/app/herramientas/crono/crono.page.ts
@@ -1,26 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-crono',
   templateUrl: './crono.page.html',
   styleUrls: ['./crono.page.scss'],
 })
-export class CronoPage implements OnInit {
+export class CronoPage implements OnInit, OnDestroy {
+
+  private stopwatch!: Subscription;
+  public started = false;
+  public paused = false;
+
+  public horas: number = 0;
+  public minutos: number = 0;
+  public segundos: number = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.pause();
+  }
+
   play() {
-
+    this.started = true
+    this.stopwatch = interval(1000).subscribe(() => {
+      this.segundos++
+      if (this.segundos == 60) {
+        this.segundos = 0
+        this.minutos++
+        if (this.minutos == 60) {
+          this.minutos = 0
+          this.horas++
+        }
+      }
+    });
   }
-  
-  stop() {
 
+  pause() {
+    this.started = false
+    if (this.stopwatch) {
+      this.stopwatch.unsubscribe();
+    }
   }
-  
+
   reset() {
+    this.started = false
+    this.pause()
+    
+    this.horas = 0;
+    this.minutos = 0;
+    this.segundos = 0;
+  }
 
+  formatElapsedTime() {
+    return `${this.horas < 10 ? '0' + this.horas : '' + this.horas}:${this.minutos < 10 ? '0' + this.minutos : '' + this.minutos}:${this.segundos < 10 ? '0' + this.segundos : '' + this.segundos}`;
   }
 }
--- src/app/herramientas/timer/timer.page.ts
+++ src/app/herramientas/timer/timer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
@@ -7,20 +8,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerPage implements OnInit {
 
+  public started = false
+  public cannotPlay = true
+  public finished = false
+
+  private stopwatch!: Subscription;
+
+  public horas: number = 0;
+  public minutos: number = 0;
+  public segundos: number = 0;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.stop();
+  }
+
   play() {
+    this.started = true
+    this.cannotPlay = true
 
+    this.stopwatch = interval(1000).subscribe(() => {
+
+
+      if (this.segundos > 0) {
+        this.segundos--
+      } else {
+        if (this.minutos > 0) {
+          this.segundos = 59
+          this.minutos--
+        } else {
+          if (this.horas > 0) {
+            this.segundos = 59
+            this.minutos = 59
+            this.horas--
+          }
+        }
+      }
+
+      if (this.horas <= 0 && this.minutos <= 0 && this.segundos <= 0) {
+        this.finished = true
+        this.stop()
+      }
+
+    });
   }
-  
+
   stop() {
-
+    if (this.stopwatch) {
+      this.stopwatch.unsubscribe();
+    }
   }
-  
-  reset() {
 
+  reset() {
+    this.stop()
+    this.started = false
+    this.cannotPlay = true
+
+    this.horas = 0;
+    this.minutos = 0;
+    this.segundos = 0;
+  }
+
+  formatElapsedTime() {
+    return `${this.horas < 10 ? '0' + this.horas : '' + this.horas}:${this.minutos < 10 ? '0' + this.minutos : '' + this.minutos}:${this.segundos < 10 ? '0' + this.segundos : '' + this.segundos}`;
+  }
+
+  onInputHoras($event: any) {
+    this.horas = $event.detail.value ? $event.detail.value : 0
+    this.cannotPlay = false
+  }
+  onInputMin($event: any) {
+    this.minutos = $event.detail.value ? $event.detail.value : 0
+    this.cannotPlay = false
+  }
+  onInputSeg($event: any) {
+    this.segundos = $event.detail.value ? $event.detail.value : 0
+    this.cannotPlay = false
   }
 }
