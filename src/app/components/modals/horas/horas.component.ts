cefb43ae717610b61699580c35b74ab00f49390a
Fernando Jose Amoedo Miguez <ferjamoedom@gmail.com>
2023-11-10 00:41:20 +0100

    agregado: -hora de fin podia ser menor que la de inicio

--- src/app/components/modals/horas/horas.component.ts
+++ src/app/components/modals/horas/horas.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { UtilsService } from '../../../services/utils.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { User } from 'firebase/auth';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
@@ -10,11 +10,14 @@ import { FirebaseService } from 'src/app/services/firebase.service';
   styleUrls: ['./horas.component.scss'],
 })
 export class HorasComponent implements OnInit {
-  
+
+
   form = new FormGroup({
     horainicio: new FormControl('00:00', [Validators.required]),
     horafin: new FormControl('00:10', [Validators.required]),
   })
+
+
 
   @Input() primaryColor!: string
   @Input() path!: string
@@ -30,12 +33,12 @@ export class HorasComponent implements OnInit {
     this.utilsService.dismissModal(data)
   }
   async addHora() {
-    if (this.form.valid) {
+    if (this.form.valid && this.validateTime()) {
 
       const loading = await this.utilsService.loading()
       await loading.present()
-      
-      this.path = this.path+'/horas'
+
+      this.path = this.path + '/horas'
 
       this.firebaseService.addDocument(this.path, this.form.value).then(async res => {
 
@@ -55,10 +58,18 @@ export class HorasComponent implements OnInit {
             message: "Debe tener ambas horas",
             duration: 1000
           })
-        }).finally(()=> {
+        }).finally(() => {
           window.location.reload()
           loading.dismiss()
         })
+    }
+  }
+  
+  validateTime(): boolean {
+    if (this.form.controls.horafin.value.split(':') < this.form.controls.horainicio.value.split(':')) {
+      return false;
+    } else {
+      return true;
     }
   }
 }
